342b72f24f4eeef8565998ba7cd75b9efae52811
Mahadi Dwi Nugraha <mahadidwinugraha@gmail.com>
2023-08-26 21:27:13 +0700

    Initial commit

---
+++ Buku/scriptBuku.js
@@ -0,0 +1,69 @@
+const bukuSejarah = {
+    "judul": [
+        "Sejarah Terlengkap Peradaban Dunia", "Ensiklopedia Sejarah Islam", 
+        "Mein Kampf", "Sejarah Dunia yang Disembunyikan"
+    ],
+    "penulis": [
+        "Rizem Aizid", "DR. Raghib As-Sirjani", "Adolf Hitler", "Jonathan Black"
+    ],
+    "penerbit": [
+        "Noktah", "Al-Kautsar", "Tidak Ada", "Tidak Ada"
+    ],
+    "url": [
+        "./bukuSejarah/bukuSejarah.jpg", "./bukuSejarah/Ensiklopedi-Sejarah-Islam.jpg", 
+        "./bukuSejarah/meinKampf.jpg", "./bukuSejarah/sejarahDunia.jpg"
+    ]
+}
+
+const bukuAgama = {
+    "judul": [
+        "Fiqih Ibadah Madzhab Syafi'i", "Minhajul Muslim", 
+        "Sirah Nabawiyah"
+    ],
+    "penulis": [
+        "Syaikh DR. Alauddin Za'tari", "Syaikh Abu Bakar Jabir Al Jazairy",
+        "Syaikh Shafiyyurrahman Al-Mubarakfuri"
+    ],
+    "Penerbit": [
+        "Al-Kautsar", "Ummul Qura", "Al-Kautsar"
+    ],
+    "url": [
+        "./bukuAgama/fiqih.jpg", "./bukuAgama/minhajulMuslim.jpg", "./bukuAgama/sirahNabawiyah.jpg"
+    ]
+}
+
+const bukuNovel = {
+    "judul": [
+        "Bumi", "Laskar Pelangi", "Negeri Para Pedebah", "Orang-Orang Biasa", 
+        "Tenggelamnya Kapal Van der Wijck"
+    ],
+    "penulis": [
+        "Tere Liye", "Andrea Hirata", "Tere Liye", "Andrea Hirata", "Hamka"
+    ],
+    "penerbit": [
+        "Gramedia", "Gramedia", "Gramedia", "Gramedia", "Gema Insani"
+    ],
+    "url": [
+        "./bukuNovel/bumi.jpg", "./bukuNovel/laskarpelangi.jpg", "./bukuNovel/negeriparapedebah.jpg",
+        "./bukuNovel/orang-orang-biasa.jpg", "./bukuNovel/TenggelamnyaKapalvanderwick.jpg"
+    ]
+}
+
+const bukuSains = {
+    "judul": [
+        "Brief History of Time", "Ensiklopedia Sains", "Pengantar Fisika Modern", 
+        "Relativitas"
+    ],
+    "penulis": [
+        "Stephen Hawking", "Tidak Ada", "Prof.Dr.Tomo Djujin", "Albert Einstein"
+    ],
+    "penerbit": [
+        "Gramedia", "Gramedia", "D", "Gramedia"
+    ],
+    "url": [
+        "./bukuSains/briefHistoryofTime.jpg", "./bukuSains/ensiklopediaSains.jpg", 
+        "./bukuSains/pengantarFisika.jpg", "./bukuSains/Relativitas.jpg"
+    ]
+}
+
+export {bukuSejarah, bukuAgama, bukuNovel, bukuSains}
